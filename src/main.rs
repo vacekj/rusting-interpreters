85a5ba810bc35769894ed7987301598072bcfdb3
Atris <vacekj@outlook.com>
2023-09-17 10:22:27 +0200

    feat: advancing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{env, fmt, fs};
+use std::ascii::Char;
 use std::process;
 use std::io;
 use std::io::{BufRead, Write};
@@ -6,7 +7,7 @@ use std::path::Path;
 use std::process::exit;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::atomic::Ordering::Relaxed;
-use crate::TokenType::Eof;
+use crate::TokenType::{Eof, LeftParen};
 
 // Global flag to indicate if an error has occurred
 static HAD_ERROR: AtomicBool = AtomicBool::new(false);
@@ -79,7 +80,7 @@ impl Scanner {
             tokens: vec![],
             start: 0,
             current: 0,
-            line: 1
+            line: 1,
         }
     }
 
@@ -88,15 +89,36 @@ impl Scanner {
     }
 
     pub fn scan_tokens(&mut self) -> &Vec<Token> {
-
         while !&self.is_at_end() {
-            &self.start = &self.current;
-            scan_token();
+            self.start = self.current.clone();
+            self.scan_token();
         }
 
         self.tokens.push(Token::new(Eof, String::from(""), String::from(""), 0));
 
         &self.tokens
+    }
+
+    fn scan_token(&mut self) {
+        let c: char = self.advance();
+        match c {
+            '(' => self.add_token(LeftParen),
+            _ => unreachable!()
+        }
+    }
+
+    fn add_token(&mut self, ty: TokenType) {
+        let text = &self.source.as_str()[self.start.clone()..self.current.clone()];
+        self.tokens.push(Token::new(ty,
+                                    text.to_string(),
+                                    text.to_string(),
+                                    self.line.clone()));
+    }
+
+    fn advance(&mut self) -> char {
+        let char_ = self.source.chars().nth(self.current.clone());
+        self.current += 1;
+        char_.unwrap()
     }
 }
 
@@ -167,11 +189,11 @@ struct Token {
     ty: TokenType,
     lexeme: String,
     literal: String,
-    line: u32,
+    line: usize,
 }
 
 impl Token {
-    pub fn new(ty: TokenType, lexeme: String, literal: String, line: u32) -> Token {
+    pub fn new(ty: TokenType, lexeme: String, literal: String, line: usize) -> Token {
         Token {
             literal,
             ty,
