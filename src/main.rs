1623362fe426883a04bad59bc774380eb0314e43
Atris <vacekj@outlook.com>
2023-09-17 09:33:06 +0200

    feat: token types

--- src/main.rs
+++ src/main.rs
@@ -80,7 +80,55 @@ impl Scanner {
 }
 
 #[derive(Debug)]
-enum Token {}
+enum Token {
+    /*Single-char tokens*/
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    /*One or two-char tokens*/
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    /*Literals*/
+    Ident,
+    String,
+    Number,
+
+    /*Keywords*/
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Eof,
+}
 
 fn error(line: usize, message: &str) {
     report(line, "", message);
